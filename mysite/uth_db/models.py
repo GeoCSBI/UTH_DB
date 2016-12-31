a9154d65d746bca33e0168dddd3517b592525109
GeoCSBI <geodimas.csbi@gmail.com>
2016-12-31 17:04:24 +0200

    Added table on db TABLES

--- mysite/uth_db/models.py
+++ mysite/uth_db/models.py
@@ -2,6 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Tables(models.Model):
+
+	seats = models.IntegerField(default=4 ,null=False)
+	isBooked = models.BooleanField(default=False, null=False)
+
+	def __str__(self):
+		return self.id + ' - ' + self.seats 
+
 class Menu(models.Model):
 
 	foodName = models.CharField(max_length=40, null=False)
